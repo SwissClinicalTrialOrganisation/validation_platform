479488b0f317250bcd8e5ef37d35de8d1855b564
Alan Haynes <aghaynes@users.noreply.github.com>
2024-04-03 12:58:04 +0200

    add template code

--- resources/calculate_risk.R
+++ resources/calculate_risk.R
@@ -4,9 +4,31 @@
 # 
 
 # path to json with info on the triggering issue
-issue <- Sys.getenv("NUMBER")
-print(issue)
+issuenum <- Sys.getenv("NUMBER")
 
+
+issue <- get_issue(issuenum)
+
+if(is_package(list(issue))){
+  score <- calculate_pkg_score(list(issue))
+
+  val <- validate_pkg_issue(score)
+
+  if(val$score_ok){
+    gh_message <- paste0("Package ", score$package, " has a score of ",
+                         round(score$final_score, 3), " which makes it a **",
+                         as.character(score$final_score_cat),
+                         "** risk package.", "\n\n",
+                         ":sparkles: Thank you for your contribution! :sparkles:")
+    can_close <- TRUE
+  } else {
+    gh_message <- val$message
+    can_close <- FALSE
+  }
+}
+
+print(gh_message)
+print(can_close)
 
 
 
