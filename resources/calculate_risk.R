11380ccf052e82a90bfad8e2433bab947070255c
Alan Haynes <aghaynes@users.noreply.github.com>
2024-04-08 15:10:35 +0200

    add support for tests

--- resources/calculate_risk.R
+++ resources/calculate_risk.R
@@ -23,9 +23,6 @@ if(validation:::is_package(list(issue))){
                          ":sparkles: Thank you for your contribution! :sparkles:")
     can_close <- TRUE
 
-	  pkgs <- validation::update_pkg_table(pkg = list(issue))
-	  readr::write_csv(pkgs, "tables/validated_packages.csv")
-
     validation:::add_label(issuenum, paste(as.character(score$final_score_cat), "risk")) |>
       try() |>
       print()
@@ -36,6 +33,11 @@ if(validation:::is_package(list(issue))){
       try() |>
       print()
     # validation:::close_issue(issuenum)
+
+
+    issue <- validation:::get_issue(issuenum)
+	  pkgs <- validation::update_pkg_table(pkg = list(issue))
+	  readr::write_csv(pkgs, "tables/validated_packages.csv")
 
   } else {
     gh_message <- val$message
@@ -52,13 +54,54 @@ if(validation:::is_test(list(issue))){
   val <- validation:::validate_test_issue(tmp)
 
   if(val$ok){
-    gh_message <- ":sparkles: Thank you for your contribution! :sparkles:"
-    can_close <- TRUE
+
+    if(tmp$test_result == "PASS"){
+      gh_message <- "Tests have passed!\n\n:sparkles: Thank you for your contribution! :sparkles:"
+
+      if(is_(list(issue), "FAIL") & tmp$test_result == "PASS"){
+        validation:::remove_label(issuenum, "FAIL") |>
+          try() |>
+          print()
+      }
+      validation:::remove_label(issuenum, ":alarm_clock: triage :alarm_clock:") |>
+        try() |>
+        print()
+
+      can_close <- TRUE
+
+
+    } else {
+      gh_message <- paste0(
+        "Tests have failed!",
+        "\n\n",
+        "Please investigate:",
+        "\n\n",
+        " - do the tests need correcting?",
+        "\n\n",
+        " - has the package changed such that the tests require updating?",
+        "\n\n",
+        " - is there a bug in the package? If so, consider making a bug report to the package developer. Post a link to the issue here.",
+        collapse = "\n\n")
+
+    }
+
+    validation:::add_label(issuenum, tmp$test_result) |>
+      try() |>
+      print()
+
+
+
+    tests <- validation::update_tests_table(tests = list(issue))
+    readr::write_csv(tests, "tables/package_tests.csv")
+
+
   } else {
     gh_message <- val$message
     can_close <- FALSE
   }
 }
+
+if(can_close) validation:::close_issue(issuenum)
 
 print(gh_message)
 print(can_close)
