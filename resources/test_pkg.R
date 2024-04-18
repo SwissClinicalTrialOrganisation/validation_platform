429a64386da189b88a97cb6245c38224aa6e639a
Alan Haynes <aghaynes@users.noreply.github.com>
2024-04-18 08:19:41 +0200

    set labels, update tests table

--- resources/test_pkg.R
+++ resources/test_pkg.R
@@ -3,11 +3,30 @@
 library(validation)
 # tests <- load_tests_table()
 
-to_test <- "accrualPlot"
+# issuenum <- Sys.getenv("NUMBER")
+# issue <- get_issue(issuenum)
+# issue_body <- issue$body
+# package <-
 
+to_test <- "accrualPlot"
 
 xx <- test(to_test, testthat_colours = FALSE)
 
 texts <- test_to_text(xx)
+
+cat("Texts prepared\n")
+
 issue <- post_issue(texts$issue_body, texts$issue_title)
-map(texts$issue_tags, ~ add_label(issue$number, .x))
+
+cat("issue posted\n")
+
+lapply(texts$issue_tags, ~ add_label(issue$number, .x))
+
+cat("labels set")
+
+cat("Updating table\n")
+Sys.sleep(10)
+pkgs <- validation::update_pkg_table()
+
+cat("Writing table\n")
+readr::write_csv(pkgs, "tables/validated_packages.csv", na = "")
