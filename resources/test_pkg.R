ebfb25d01a516d3bb98445e0dee18c1f83e1a5c5
Alan Haynes <aghaynes@users.noreply.github.com>
2024-04-19 08:35:13 +0200

    all tests in one issue

--- resources/test_pkg.R
+++ resources/test_pkg.R
@@ -8,7 +8,12 @@ library(validation)
 # issue_body <- issue$body
 # package <-
 
-to_test <- "accrualPlot"
+issuenum <- Sys.getenv("NUMBER")
+issue <- get_issue(issuenum)
+title <- issue$title
+pkg <- gsub("^[Run test]", "") |> trimws()
+
+to_test <- pkg
 
 xx <- test(to_test, testthat_colours = FALSE)
 
@@ -16,12 +21,12 @@ texts <- test_to_text(xx)
 
 cat("Texts prepared\n")
 
-issue <- post_issue(texts$issue_body, texts$issue_title)
+issue <- post_comment(issuenum, texts$issue_body)
 
-cat("issue posted\n")
+cat("issue updated\n")
 
-lapply(texts$issue_tags, function(x) add_label(issue = issue$number, label = x))
-
-cat("labels set")
+# lapply(texts$issue_tags, function(x) add_label(issue = issue$number, label = x))
+#
+# cat("labels set")
 
 
